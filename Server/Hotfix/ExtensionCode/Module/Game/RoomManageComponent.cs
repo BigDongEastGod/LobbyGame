f76cdc05a41daff137ec509b57953434d2beea3c
Unknown <362946@qq.com>
2018-04-27 14:32:21 +0800

    RoomManageComponent逻辑部分转移到Hotfix层

---
+++ Server/Hotfix/ExtensionCode/Module/Game/RoomManageComponent.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using ETModel;
+
+namespace ETHotfix
+{
+    /// <summary>
+    /// 房间组件扩展类
+    /// </summary>
+    public static class RoomManageComponentEx
+    {
+        public static bool Add(this RoomManageComponent self,long roomId,long playerId,Room room)
+        {
+            if (self.Rooms.Any(d => d.PlayerId == playerId)) return false;
+
+            self.Rooms.Add(new RoomManageComponent.RoomModel() {RoomId = roomId, PlayerId = playerId, Room = room});
+
+            return true;
+        }
+
+        public static Room GetRoom(this RoomManageComponent self,long roomId)
+        {
+            return self.Rooms.FirstOrDefault(d=>d.RoomId==roomId)?.Room;
+        }
+
+        public static long? GetRoomId(this RoomManageComponent self,Room room)
+        {
+            return self.Rooms.FirstOrDefault(d => d.Room == room)?.RoomId;
+        }
+
+        public static void Remove(this RoomManageComponent self,Room room)
+        {
+            var removeroom = self.Rooms.FirstOrDefault(d => d.Room == room);
+
+            if (removeroom == null) return;
+            
+            removeroom.Room.DissolveRoom();
+
+            self.Rooms.Remove(removeroom);
+        }
+
+        public static void Remove(this RoomManageComponent self,long roomId)
+        {
+            var removeroom = self.Rooms.FirstOrDefault(d => d.RoomId == roomId);
+            
+            if (removeroom == null) return;
+            
+            removeroom.Room.DissolveRoom();
+
+            self.Rooms.Remove(removeroom);
+        }
+    }
+}
--- Server/Model/ExtensionCode/Module/Game/RoomManageComponent.cs
+++ Server/Model/ExtensionCode/Module/Game/RoomManageComponent.cs
@@ -26,54 +26,13 @@ namespace ETModel
         
         public static RoomManageComponent Instance;
         
-        private List<RoomModel> _rooms;
+        public List<RoomModel> Rooms;
         
         public void Awake()
         {
             Instance = this;
 
-            _rooms = new List<RoomModel>();
-        }
-
-        public bool Add(long roomId,long playerId,Room room)
-        {
-            if (_rooms.Any(d => d.PlayerId == playerId)) return false;
-
-            _rooms.Add(new RoomModel() {RoomId = roomId, PlayerId = playerId, Room = room});
-
-            return true;
-        }
-
-        public Room GetRoom(long roomId)
-        {
-            return _rooms.FirstOrDefault(d=>d.RoomId==roomId)?.Room;
-        }
-
-        public long? GetRoomId(Room room)
-        {
-            return _rooms.FirstOrDefault(d => d.Room == room)?.RoomId;
-        }
-
-        public void Remove(Room room)
-        {
-            var removeroom = _rooms.FirstOrDefault(d => d.Room == room);
-
-            if (removeroom == null) return;
-            
-            removeroom.Room.DissolveRoom();
-
-            _rooms.Remove(removeroom);
-        }
-
-        public void Remove(long roomId)
-        {
-            var removeroom = _rooms.FirstOrDefault(d => d.RoomId == roomId);
-            
-            if (removeroom == null) return;
-            
-            removeroom.Room.DissolveRoom();
-
-            _rooms.Remove(removeroom);
+            Rooms = new List<RoomModel>();
         }
 
         public override void Dispose()
@@ -82,9 +41,9 @@ namespace ETModel
 
             base.Dispose();
             
-            _rooms.ForEach(d=>d.Room.DissolveRoom());
+            Rooms.ForEach(d=>d.Room.DissolveRoom());
             
-            _rooms.Clear();
+            Rooms.Clear();
         }
     }
 }
