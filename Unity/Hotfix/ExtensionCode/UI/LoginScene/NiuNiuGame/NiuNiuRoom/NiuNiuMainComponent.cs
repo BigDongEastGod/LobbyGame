53db1d343dddee363534532ec27f3374a8a2f416
Unknown <657436642@qq.com>
2018-04-30 17:25:36 +0800

    增加了进入房间获取准备玩家
    修改了设置坐下的函数

--- Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuRoom/NiuNiuMainComponent.cs
+++ Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuRoom/NiuNiuMainComponent.cs
@@ -19,11 +19,13 @@ namespace ETHotfix
     
     public class NiuNiuMainComponent:Component
     {
-        private Text roomNum;               //房间号码文本
-        private long roomId;                //房间号码
-        private UI showCardUI;              //卡牌存放窗口
-        private Button sitDownBt;           //坐下按钮
-        private Button startGameBt;         //开始按钮
+        private Text roomNum;                        //房间号码文本
+        private long roomId;                         //房间号码
+        private UI showCardUI;                       //卡牌存放窗口
+        private Button sitDownBt;                    //坐下按钮
+        private Button startGameBt;                  //开始按钮
+        public GetAccountInfoResponse Player;        //当前玩家数据
+        public RoomInfoResponse roomInfo;            //房间信息
         
        public async void Awake()
         {
@@ -39,7 +41,6 @@ namespace ETHotfix
             var bottomScoreText  = rc.Get<GameObject>("BottomScoreText").GetComponent<Text>();
             //房间局数
             var roomCountText  = rc.Get<GameObject>("roomCountText").GetComponent<Text>();
-           
             //房间信息按钮
             var roomInfoButton  = rc.Get<GameObject>("RoomInfoButton");
             //下拉窗口按钮
@@ -74,27 +75,35 @@ namespace ETHotfix
             var readyButton=rc.Get<GameObject>("readyButton");
             //表情按钮
             var exitButton=rc.Get<GameObject>("ExitButton");
-            //时间 电池 wifi
+            //时间
             var timeTxt=rc.Get<GameObject>("TimeTxt");
+            //电池
             var batterySlider=rc.Get<GameObject>("BatterySlider");
+            //wifi
             var wiFiImg=rc.Get<GameObject>("WiFiImg");
+            //自动翻牌
             var AutomaticFlopToggle=rc.Get<GameObject>("AutomaticFlopToggle");
-            
+            //请求获得当前账号数据
+            var accountResponse = (GetAccountInfoResponse) await SceneHelperComponent.Instance.Session.Call(new GetAccountInfoRequest());
+            Player = accountResponse;
+            //请求获得当前房间准备好的玩家信息
             var response =(RoomInfoResponse) await SceneHelperComponent.Instance.Session.Call(new RoomInfoRequest() {RoomId = 0, Message = -1});
-    
+            //获得房间规则信息
             var rules = response.Rules == null ? null : ProtobufHelper.FromBytes<NNChess>(response.Rules);
-
+            //设置房间号
             roomId = response.RoomId;
             roomNum.text = response.RoomId == 0 ? null : roomId.ToString();
-             //zhuangWeiTxt=rules.
-            bottomScoreText.text = rules.Score.ToString();
-            roomCountText.text = rules.Dish.ToString();
+            //TODo.....庄位信息
+            //zhuangWeiTxt=rules.
             
-            RoomInfoAnnunciateHandler.RoomAction += RoomInfo;
+            //设置房间底分
+            bottomScoreText.text = rules.Score.ToString();
+            //设置房间局数
+            roomCountText.text = "1/"+rules.Dish.ToString();
             //获得房间显示卡牌窗口
             showCardUI=Game.Scene.GetComponent<UIComponent>().Create(UIType.NNShowCard, UiLayer.Medium);
             //设置房间人数
-            showCardUI.GetComponent<NNShowCardComponent>().roomPeople = 6;
+            showCardUI.GetComponent<NNShowCardComponent>().roomPeople =(ushort)rules.PlayerCount;
             //加载所需要的位置信息
             showCardUI.GetComponent<NNShowCardComponent>().GetCurrentTablePos();
             
@@ -117,8 +126,11 @@ namespace ETHotfix
                 {
                     GetRoomInfo();
                 });
-        
+
+            //获取房间准备号玩家的数据
+            GetAllReadyInfo();
             
+            RoomInfoAnnunciateHandler.RoomAction += RoomInfo;
 
         }
 
@@ -136,11 +148,22 @@ namespace ETHotfix
                 Debug.Log("成功坐下");
                 sitDownBt.gameObject.SetActive(false);
                 startGameBt.GetComponent<Animator>().SetInteger("IsMiddle",1);
-                showCardUI.GetComponent<NNShowCardComponent>().CreateHead(-1);
+                foreach (var player in roomInfo.Players)
+                {
+                    if(player.UserName == Player.AccountInfo.UserName)
+                        showCardUI.GetComponent<NNShowCardComponent>().CreateHead(-1,player);
+                }
             }
         }
-        
-        
-       
+
+        private void GetAllReadyInfo()
+        {
+            for (int i = 0; i < roomInfo.Players.Count; i++)
+            {
+                showCardUI.GetComponent<NNShowCardComponent>().CreateHead(i,roomInfo.Players[i]);
+            }
+        }
+
+
     }
 }
