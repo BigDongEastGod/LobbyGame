3c7d78bb3c014ee2b642b0b47c70ad2391a1a030
Fist <zlcn75@gmail.com>
2018-05-01 14:55:10 +0800

    给游戏场景传入RoomId

--- Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuLobby/NiuNiuJoinRoom/NiuNiuJoinRoomComponent.cs
+++ Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuLobby/NiuNiuJoinRoom/NiuNiuJoinRoomComponent.cs
@@ -126,7 +126,7 @@ namespace ETHotfix
             {
                 Debug.Log("加入房间成功,跳转至游戏主场景");
 
-                Game.Scene.GetComponent<UIComponent>().Create(UIType.NiuNiuMain, UiLayer.Bottom);
+                Game.Scene.GetComponent<UIComponent>().Create(UIType.NiuNiuMain, UiLayer.Bottom, paijuNumber);
                 Game.Scene.GetComponent<UIComponent>().Remove(UIType.NiuNiuLobby);
             }
             else
