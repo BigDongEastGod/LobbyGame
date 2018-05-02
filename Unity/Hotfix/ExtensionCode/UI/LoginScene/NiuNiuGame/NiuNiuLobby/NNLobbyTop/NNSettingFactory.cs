b180c11ceee6870cb713d1e2d8af447f81008a55
Fist <zlcn75@gmail.com>
2018-05-02 18:30:56 +0800

    增加设置界面,跳转登录界面

--- Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuLobby/NNLobbyTop/NNSettingFactory.cs
+++ Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuLobby/NNLobbyTop/NNSettingFactory.cs
@@ -1,7 +1,18 @@
-﻿namespace ETHotfix
+﻿using ETModel;
+using UnityEngine;
+
+namespace ETHotfix
 {
-    public class NNSettingFactory
+    [UIFactory(UIType.NiuNiuSetting)]
+    public class NNSettingFactory:AUIFactory
     {
-        
+        public override UI Create(Scene scene, string type, GameObject parent)
+        {
+            var ui = base.Create(scene, type, parent);
+
+            ui.AddComponent<NNSettingComponent>();
+
+            return ui;
+        }
     }
 }
