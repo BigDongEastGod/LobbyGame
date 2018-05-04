8ebd82bdb39d50705ae25344348d9868c0ad3c56
Unknown <362946@qq.com>
2018-05-04 10:07:58 +0800

    22

---
+++ Unity/Assets/Scripts/ExtensionCode/Helper/ETDoTween.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using DG.Tweening.Core;
+using UnityEngine;
+
+namespace ETModel
+{
+    public static class ETDoTweenEx
+    {
+        public static Tweener DOMove(this Transform target, Vector3 endValue, float duration, bool snapping = false)
+        {
+            return DOTween.To((DOGetter<Vector3>) (() => target.position), (DOSetter<Vector3>) (x => target.position = x), endValue, duration).SetOptions(snapping).SetTarget<Tweener>((object) target);
+        }
+    }
+}
