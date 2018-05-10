8feb606aee911e361e76ac2c75af8129cdbbe4c3
Unknown <657436642@qq.com>
2018-05-10 12:26:21 +0800

    1

--- Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuRoom/NiuNiuMainComponent.cs
+++ Unity/Hotfix/ExtensionCode/UI/LoginScene/NiuNiuGame/NiuNiuRoom/NiuNiuMainComponent.cs
@@ -256,6 +256,9 @@ namespace ETHotfix
                       FlopOtherCard(otherPokerList.PokerCards.ToList(),obj.UserName);
                       _showCardUi.GetComponent<NnShowCardComponent>().ShowTipsUi(obj.UserName,otherPokerList.CardTypeNumber);
                       break;
+                  case 5:
+                      
+                      break;
             }
         }
         
@@ -382,14 +385,10 @@ namespace ETHotfix
         private async void OnBrightCardButtonClick()
         {
             var calculateCardResponse =(CalculateCardResponse) await SceneHelperComponent.Instance.Session.Call(new CalculateCardRequest() {RoomId = _mRoomId});
-            if (calculateCardResponse.Error == 0)
-            {
-                SwitchTipsCard(false);
-            }
+            if (calculateCardResponse.Error != 0) return;
+            _showCardUi.GetComponent<NnShowCardComponent>().ShowTipsUi(_player.AccountInfo.UserName);
+            SwitchTipsCard(false);
         }
-
-
-
 
         private void Test()
         {
@@ -410,7 +409,7 @@ namespace ETHotfix
             List<PokerCard> list=new List<PokerCard>();
             for (var i = 0; i < 5; i++)
             {
-                PokerCard temp = new PokerCard
+                var temp = new PokerCard
                 {
                     CardNumber = i,
                     CardType = 0
